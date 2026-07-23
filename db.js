78641fde6de1213d5b47bc6b4f8cc99aa5982e10
FoggyLobster <bentleytenges@gmail.com>
2026-07-22 21:57:38 -0500

    A

--- db.js
+++ db.js
@@ -11,20 +11,22 @@ if (!fs.existsSync(dataPath)) {
 const db = new Database(path.join(dataPath, "database.db"));
 
 db.prepare(
-  `CREATE TABLE IF NOT EXISTS quarantine (
+  `
+  CREATE TABLE IF NOT EXISTS quarantine (
     user_id TEXT PRIMARY KEY,
     previous_roles TEXT NOT NULL,
     reason TEXT NOT NULL
-);
+  );
 `,
 ).run();
 
 db.prepare(
-  `CREATE TABLE IF NOT EXISTS staff_results (
+  `
+  CREATE TABLE IF NOT EXISTS staff_results (
     user_id TEXT PRIMARY KEY,
     result TEXT NOT NULL,
     notes TEXT NOT NULL
-);
+  );
 `,
 ).run();
 
@@ -33,12 +35,12 @@ db.prepare(
   CREATE TABLE IF NOT EXISTS shifts (
     id INTEGER PRIMARY KEY,
     user_id TEXT NOT NULL,
-    status TEXT NOT NULL DEFAULT "offline",
+    status TEXT NOT NULL DEFAULT 'offline',
     started_at INTEGER NOT NULL,
     total_time INTEGER NOT NULL,
-    total_shifts INTEGER NOT NULL,
-);
-  `,
+    total_shifts INTEGER NOT NULL
+  );
+`,
 ).run();
 
 db.prepare(
@@ -47,9 +49,9 @@ db.prepare(
     id INTEGER PRIMARY KEY,
     user_id TEXT NOT NULL,
     started_at INTEGER NOT NULL,
-    ended_at INTEGER NOT NULL,
-);
-  `,
-);
+    ended_at INTEGER NOT NULL
+  );
+`,
+).run();
 
 module.exports = db;
