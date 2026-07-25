b4f0dac626baa39894d0e84d8ab880c5563b6020
FoggyLobster <bentleytenges@gmail.com>
2026-07-25 13:30:21 -0500

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,32 @@
+
+
+
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+  name: "guildMemberAdd",
+
+  async execute(client, member) {
+    const channel = client.channels.cache.get("1313985498397675582");
+
+    if (!channel) return;
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("members")
+        .setLabel(member.guild.memberCount.toString())
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true),
+        .setEmoji(“1530640507577831676”)
+    );
+
+    await channel.send({
+      content: `Welcome, ${member} to ** <:lakeshore:1530640507577831676> Lakeshore County Roleplay!** You are member \`#${member.guild.memberCount}\`.`,
+      components: [row],
+    });
+    const role = member.roles.cache.get("1529307210851684503");
+    if (role) {
+      await member.roles.add(role);
+    }
+  },
+};
