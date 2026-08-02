88b19f75db47197b7a2ab9a2a16c29aeca4c6587
FoggyLobster <bentleytenges@gmail.com>
2026-08-02 18:40:16 -0500

    A

--- buttons/sessionsButton.js
+++ buttons/sessionsButton.js
@@ -2,26 +2,24 @@ module.exports = {
   customId: "session_role",
 
   async execute(interaction) {
-    const role_id = interaction.guild.roles.cache.get("1533618970807238798");
+    const role_id = "1533618970807238798";
 
     const alreadyHasRole = interaction.member.roles.cache.has(role_id);
 
     if (alreadyHasRole) {
-      interaction.member.roles.remove(role_id);
+      await interaction.member.roles.remove(role_id);
 
-      interaction.reply({
+      return interaction.reply({
         content: "Removed the sessions role successfully.",
         ephemeral: true,
       });
-      return;
-    } else {
-      interaction.member.roles.add(role_id);
-
-      return interaction.reply({
-        content: "Added the sessions role successfully.",
-        ephemeral: true,
-      });
-      return;
     }
+
+    await interaction.member.roles.add(role_id);
+
+    return interaction.reply({
+      content: "Added the sessions role successfully.",
+      ephemeral: true,
+    });
   },
 };
